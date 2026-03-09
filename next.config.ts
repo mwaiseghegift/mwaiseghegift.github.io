f6d7ac2420377b4cf5fa838da4777e84a57bc035
mwaiseghegift <gift.mwaiseghe@griffinglobaltech.com>
2026-03-09 11:25:49 +0300

    feat: Update next.config.ts to support GitHub Pages with conditional output and image optimization

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+
 const nextConfig: NextConfig = {
+  output: isGithubPages ? 'export' : undefined,
   images: {
+    unoptimized: isGithubPages,
     remotePatterns: [
       {
         protocol: 'https',
